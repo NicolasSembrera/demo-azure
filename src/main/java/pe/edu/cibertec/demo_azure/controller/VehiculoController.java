6f66c446bb45287878c63ef1693273fc891a4b89
NicolasSembrera <nsembrera@gmail.com>
2024-10-18 20:18:09 -0500

    Subiendo Proyecto

---
+++ src/main/java/pe/edu/cibertec/demo_azure/controller/VehiculoController.java
@@ -0,0 +1,29 @@
+package pe.edu.cibertec.demo_azure.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class VehiculoController {
+
+    @GetMapping("/vehiculos")
+    public String obtenerVehiculos(Model model){
+
+        List<String> listaVehiculo = new ArrayList<String>();
+
+        listaVehiculo.add("Toyota");
+        listaVehiculo.add("Nissan");
+        listaVehiculo.add("Ferrari");
+        listaVehiculo.add("Tesla");
+        listaVehiculo.add("Honda");
+
+        model.addAttribute("lista", listaVehiculo);
+
+        return "index";
+    }
+
+}
